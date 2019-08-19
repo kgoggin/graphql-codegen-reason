04c5cdf5e8ea728af7c1b6b2e55f69d279e9b1d8
Kyle Goggin <kyle@kylegoggin.com>
2019-08-19 16:44:57 -0400

    update all deps and add refmt option to config

--- src/index.ts
+++ src/index.ts
@@ -1,28 +1,35 @@
 import { PluginFunction, Types } from "@graphql-codegen/plugin-helpers";
 import { GraphQLSchema, printSchema, parse, visit } from "graphql";
-import { camelCase } from "lodash";
 import { parseRE, printRE } from "reason";
 import { makeVisitor } from "./visitor";
 
 export interface ReasonConfig {
   scalars?: { [scalarName: string]: string };
+  refmt?: boolean;
 }
 
 const refmt = (str: string) => printRE(parseRE(str));
 
+const defaultConfig = {
+  scalars: {},
+  refmt: true
+};
+
 export const plugin: PluginFunction<ReasonConfig> = async (
   schema: GraphQLSchema,
   documents: Types.DocumentFile[],
-  config: ReasonConfig
+  c: ReasonConfig
 ) => {
   const printedSchema = printSchema(schema);
   const astNode = parse(printedSchema);
-
+  const config = { ...defaultConfig, ...c };
   const visitor = makeVisitor(config);
 
   visit(astNode, {
     leave: visitor
   });
 
-  return visitor.write();
+  const result = visitor.write();
+
+  return config.refmt ? refmt(result) : result;
 };
